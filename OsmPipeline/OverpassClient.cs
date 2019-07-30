74e1f903b272a4081baa90d028797c040ab376d5
Alex Hennings <alex.hennings@garmin.com>
2019-07-29 23:02:25 -0400

    [master]: Getting objects from overpass and deserializing them into osmSharp.

--- OsmPipeline/OverpassClient.cs
+++ OsmPipeline/OverpassClient.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace OsmPipeline
 {
+	// Day Limit:10,000 queries, 5 gb
 	public static class OverpassAPI
 	{
-		// Day Limit:10,000 queries, 5 gb
 		private const string Url = "http://overpass-api.de/api/interpreter";
-		private static XmlSerializer Deserializer = new XmlSerializer(typeof(osmEntity));
 
-		public static async Task<osmEntity> Fetch(string query)
+		public static async Task<OsmSharp.API.Osm> Fetch(string query)
+		{
+			var serializer = new XmlSerializer(typeof(OsmSharp.API.Osm));
+			var stream = await FetchStream(query);
+			var res = serializer.Deserialize(stream) as OsmSharp.API.Osm;
+			return res;
+		}
+
+		public static async Task<string> FetchAsString(string query)
+		{
+			return new StreamReader(await FetchStream(query)).ReadToEnd();
+		}
+
+		private static async Task<Stream> FetchStream(string query)
 		{
 			query = query.Replace("+", @"%2B");
 			//query = query.Replace("/", @"%2F");
@@ -25,7 +39,8 @@ namespace OsmPipeline
 			request.Content = content;
 			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
 			request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US"));
-			request.Headers.UserAgent.Add(new ProductInfoHeaderValue("Pipeline-OverpassClient", "0.1"));
+			request.Headers.UserAgent.Add(new ProductInfoHeaderValue("OsmPipeline", "0.1"));
+			//request.Headers.Referrer = new Uri("https://github.com/blackboxlogic/OsmPipeline");
 			var response = await client.SendAsync(request);
 
 			if (!response.IsSuccessStatusCode)
@@ -34,10 +49,8 @@ namespace OsmPipeline
 			}
 
 			var responseStream = await response.Content.ReadAsStreamAsync();
-			//Console.WriteLine(new System.IO.StreamReader(responseStream).ReadToEnd());
-			var osm = (osmEntity)Deserializer.Deserialize(responseStream);
 
-			return osm;
+			return responseStream;
 		}
 	}
 }
--- OsmPipeline/Program.cs
+++ OsmPipeline/Program.cs
@@ -1,33 +1,45 @@
 ﻿using System;
 using System.Linq;
+using System.Xml.Serialization;
 
 namespace OsmPipeline
 {
 	class Program
 	{
+		private const ulong westbrook = 132501 + 3600000000;
+		private const ulong maine = 63512 + 3600000000;
+		private const ulong frenchtown = 707032430 + 2400000000;
+
 		static void Main(string[] args)
 		{
 			string query =
 @"node
   ['phone'~'^([^0-9]*1)?([^0-9]*[0-9]){10}[^0-9]*$']
   ['phone'!~'^\\+1\\-[0-9]{3}\\-[0-9]{3}\\-[0-9]{4}$']
-  (area: 3600063512);
-out;";
-
+  (area: 3600132501);
+out meta;";
 			var response = OverpassAPI.Fetch(query);
 			response.Wait();
-
-			foreach (var node in response.Result.node)
+			foreach (var node in response.Result.Nodes)
 			{
-				var number = node.tag?.FirstOrDefault(t => t.k == "phone")?.v;
-				if (number != null)
-				{
-					Console.WriteLine($"{node.id}:\t{number}\t-->\t{Phones.Fix(number)}");
-					//Console.WriteLine(number);
-				}
+				var number = node.Tags["phone"];
+				var fixedPhone = Phones.Fix(number);
+				Console.WriteLine($"{node.Id}:\t{number}\t-->\t{fixedPhone}");
+				node.Tags["phone"] = fixedPhone;
 			}
 
+			//Console.ReadKey(true);
+			//get();
 			Console.ReadKey(true);
+		}
+
+		public static void get()
+		{
+
+			OsmSharp.IO.API.Client client = new OsmSharp.IO.API.Client("https://www.openstreetmap.org/api/0.6/");
+			var noder = client.GetNode("5045468981");
+			noder.Wait();
+			var node = noder.Result;
 		}
 	}
 }
