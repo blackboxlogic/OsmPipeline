0718e3f4ab6ab0b05c55bdaaafebbcae99bd75c1
Alex Hennings <blackboxlogic@gmail.com>
2020-07-06 22:03:05 -0400

    Using recursive with to expand route_num into ref and alt_name.

--- MaineRoads/Road Data/E911RoadsToOsmSchema.sql
+++ MaineRoads/Road Data/E911RoadsToOsmSchema.sql
@@ -1,3 +1,7 @@
+-- References
+-- https://www.samuelbosch.com/2018/02/split-into-rows-sqlite.html
+-- https://en.wikipedia.org/wiki/List_of_Interstate_Highways_in_Maine
+
 -- Data Corrections
 Update elements set route_num = '' where route_num = '0';
 Update elements set SPEED = '' where speed in ('0', '2', '3', '253');
@@ -5,72 +9,58 @@ Update elements set route_num = '202/4' where objectid = '145597' AND route_num 
 update elements set STREETNAME = 'Green' where objectid in ('25384', '131257') AND STREETNAME = 'Greene';
 
 -- Schema Translation
+WITH routes(xid, xtype, [ref], [alt_name]) AS (
+	-- Split RouteNum into separate records by /
+	WITH RECURSIVE route_nums(xid, xtype, route, rest) AS (
+		SELECT xid, xtype, '', route_num || '/' FROM elements WHERE route_num
+			UNION ALL
+		SELECT xid, xtype,
+			Substr(rest, 0, Instr(rest, '/')),
+			Substr(rest, Instr(rest, '/') + 1)
+		FROM route_nums
+		WHERE rest <> '')
+	-- Translate RouteNum into ref and Alt_name, and join them with ;
+	SELECT
+			xid,
+			xtype,
+			group_concat(Coalesce(prefix.value, 'ME') || ' ' || [route], ';') AS [ref],
+			group_concat(
+				Replace(
+					Replace(
+						Coalesce(prefix.value, 'Route'),
+						'I', 'Interstate'),
+					'US', 'U.S. Route') || ' ' || [route], ';') AS [alt_name]
+		FROM route_nums
+		LEFT JOIN RoutePrefixes AS prefix
+			ON route = prefix.id
+		WHERE route <> ''
+		GROUP by xid, xtype)
 SELECT
 		Elements.xid, -- Required first column
 		Elements.xtype, -- Required second column
-
 		COALESCE(pre.value || ' ', '')
 			|| STREETNAME
 			|| COALESCE(' ' || suf.value, '')
-			|| COALESCE(' ' || post.value, '') as [name],
-
+			|| COALESCE(' ' || post.value, '') AS [name],
 		CASE ONEWAY
 			WHEN 'FT' THEN 'yes'
-			WHEN 'TF' THEN '-1' -- way could be reversed, oneway changed to yes
-		END as [oneway],
-
-		-- split on /, add ME_ (or US_ for 1, 1A, 2, 2A, 201, 201A, 202, 302), join with ;
-		CASE ROUTE_NUM
-			WHEN '' THEN null
-			ELSE
-			== https://www.samuelbosch.com/2018/02/split-into-rows-sqlite.html
-				RTRIM(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-					Replace(
-						'ME ' || replace(ROUTE_NUM, '/', ';ME ') || ';',
-					-- https://en.wikipedia.org/wiki/List_of_U.S._Highways_in_Maine
-					'ME 1;', 'US 1;'),
-					'ME 1A;', 'US 1A;'),
-					'ME 2;', 'US 2;'),
-					'ME 2A;', 'US 2A;'),
-					'ME 201;', 'US 201;'),
-					'ME 201A;', 'US 201A;'),
-					'ME 202;', 'US 202;'),
-					'ME 302;', 'US 302;'),
-					-- https://en.wikipedia.org/wiki/List_of_Interstate_Highways_in_Maine
-					'ME 95;', 'I 95;'),
-					'ME 195;', 'I 195;'),
-					'ME 295;', 'I 295;'),
-					'ME 395;', 'I 395;'),
-					'ME 495;', 'I 495;')
-				, ';')
-		END as [ref],
-
+			WHEN 'TF' THEN '-1' -- TODO: reverse these ways, change to 'yes'
+		END AS [oneway],
+		Routes.ref,
+		Routes.alt_name,
 		otherTags.*, -- highway:residential could be {primary, seconday, tertiary, residential}
-
-		OBJECTID as [medot:objectid]
-
-		-- Get ref as alt name (https://en.wikipedia.org/wiki/Module:Road_data/strings/USA/ME)
-		-- Punctualize from local sources?
-		-- find matching names from other subject fields
+		OBJECTID AS [medot:objectid]
 	FROM Elements
-	LEFT JOIN Directions as pre
+	LEFT JOIN Routes
+		ON Routes.xid = Elements.xid
+			AND Routes.xtype = Elements.xtype
+	LEFT JOIN Directions AS pre
 		ON pre.id = PREDIR
-	LEFT JOIN Directions as post
+	LEFT JOIN Directions AS post
 		ON post.id = POSTDIR
-	LEFT JOIN StreetSuffixes as suf
+	LEFT JOIN StreetSuffixes AS suf
 		ON suf.id = SUFFIX
-	LEFT JOIN RoadClasses as otherTags
+	LEFT JOIN RoadClasses AS otherTags
 		ON otherTags.id = RDCLASS
-	WHERE highway != 'proposed' AND xtype = 'Way'
+	WHERE highway != 'proposed'
+		AND Elements.xtype = 'Way'
