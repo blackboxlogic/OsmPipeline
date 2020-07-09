bad25b8b42959d6810a23099ae21c5c610fd76f4
Alex Hennings <blackboxlogic@gmail.com>
2020-07-09 09:04:33 -0400

    removing alt_name

--- MaineRoads/Road Data/E911RoadsToOsmSchema.sql
+++ MaineRoads/Road Data/E911RoadsToOsmSchema.sql
@@ -8,33 +8,32 @@ Update elements set SPEED = '' where speed in ('0', '2', '3', '253');
 Update elements set route_num = '202/4' where objectid = '145597' AND route_num = '2002/4';
 update elements set STREETNAME = 'Green' where objectid in ('25384', '131257') AND STREETNAME = 'Greene';
 update elements set ONEWAY = 'FT' where objectid in ('104293') and ONEWAY is null;
+update elements set streetname = 'William L Clarke Drive' where objectid in ('6225','22450','49879','76123','6693','67108','103602','19485','59216','138476','95558','113127') and streetname = 'William B Clarke Drive';
 
 -- Schema Translation
 WITH routes(xid, xtype, [ref], [alt_name]) AS (
-	-- Split RouteNum into separate records by /
+	-- Split RouteNum by /
 	WITH RECURSIVE route_nums(xid, xtype, route, rest) AS (
 		SELECT xid, xtype, '', route_num || '/' FROM elements WHERE route_num
-			UNION ALL
-		SELECT xid, xtype,
-			Substr(rest, 0, Instr(rest, '/')),
-			Substr(rest, Instr(rest, '/') + 1)
-		FROM route_nums
-		WHERE rest <> '')
-	-- Translate RouteNum into ref and alt_name, and join them with ;
+			UNION ALL SELECT xid, xtype,
+				Substr(rest, 0, Instr(rest, '/')),
+				Substr(rest, Instr(rest, '/') + 1)
+			FROM route_nums
+			WHERE rest <> '')
+	-- Translate RouteNum into ref, and rejoin with ;
 	SELECT
 			xid,
 			xtype,
-			group_concat(Coalesce(prefix.value, (select value from RoutePrefixes where id = '*')) || ' ' || [route], ';') AS [ref],
 			group_concat(
-				Replace(
-					Replace(
-						Coalesce(prefix.value, 'Route'),
-						'I', 'Interstate'),
-					'US', 'US Route') || ' ' || [route], ';') AS [alt_name]
+				Coalesce(
+					prefix.value,
+					(select value from RoutePrefixes where id = '*'))
+				|| ' ' || [route], ';') AS [ref]
 		FROM route_nums
 		LEFT JOIN RoutePrefixes AS prefix
 			ON route = prefix.id
 		WHERE route <> ''
+		ORDER BY [route]
 		GROUP by xid, xtype)
 SELECT
 		Elements.xid, -- Required first column
@@ -45,11 +44,10 @@ SELECT
 			|| COALESCE(' ' || post.value, '') AS [name],
 		CASE ONEWAY
 			WHEN 'FT' THEN 'yes'
-			WHEN 'TF' THEN '-1' -- TODO: reverse these ways, change to 'yes'
+			WHEN 'TF' THEN '-1' -- Later: reverse these ways, change to 'yes'
 		END AS [oneway],
 		Routes.ref,
-		Routes.alt_name,
-		otherTags.*, -- highway:residential could be {primary, seconday, tertiary, residential}
+		otherTags.*, -- highway:residential could be {primary, seconday, tertiary, residential, unclassified}
 		OBJECTID AS [medot:objectid]
 	FROM Elements
 	LEFT JOIN Routes
